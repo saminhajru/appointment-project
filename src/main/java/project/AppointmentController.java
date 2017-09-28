5b2ed7aaf398ef74599d8e6c300842fda8411ff8
ewo <ewo@ewo-PC>
2017-09-28 19:14:21 +0200

    Project started

---
+++ src/main/java/project/AppointmentController.java
@@ -0,0 +1,14 @@
+package project;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class AppointmentController {
+	
+	@RequestMapping(value = "/home")
+	public String homePage() {
+		return "home";
+	}
+
+}
